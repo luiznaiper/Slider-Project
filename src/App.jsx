aa3c3f53c327b2293f098dd5fa3845ae82a28854
luiznaiper <luiz_naiper@hotmail.com>
2023-03-30 07:20:51 -0300

    :construction: Main structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FaQuoteRight } from 'react-icons/fa'
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
+import { shortList, list, longList } from './data'
+import Carousel from './Carousel'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [people, setPeople] = useState(shortList)
+
+  const prevSlide = () => {}
+  const nextSlide = () => {}
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <main>
+      <section className="slider-container">
+        {people.map((person) => {
+          const { id, image, name, title, quote } = person
+          return (
+            <article className="slide" key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h5 className="name">{name}</h5>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          )
+        })}
+        <button type="button" className="prev" onClick={prevSlide}>
+          <FiChevronLeft />
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+        <button type="button" className="next" onClick={nextSlide}>
+          <FiChevronRight />
+        </button>
+      </section>
+    </main>
   )
 }
 
